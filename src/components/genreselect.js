1d08f61be3b108a5ee2f8b0fd2d289c87af43af7
Mayur <mayurkamaliya36@gmail.com>
2023-10-19 15:34:47 +0530

    Add e2e tests using cypress

--- src/components/genreselect.js
+++ src/components/genreselect.js
@@ -6,7 +6,7 @@ class GenreSelect extends React.Component {
     const { genres, selectedGenre, onSelect } = this.props;
 
     const genreButtons = genres.map((genre) => {
-      const buttonClass = `${genre === selectedGenre ? 'red' : 'white'}`;
+      const buttonClass = `genre-button ${genre === selectedGenre ? 'red' : 'white'}`;
       return (
         <button
           key={genre}
